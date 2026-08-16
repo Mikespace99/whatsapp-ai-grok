940b65b78d486d8ca5e0e4db18cb02f946fe7eac
Mikespace99 <michele.serpico@gmail.com>
2026-08-16 18:42:49 +0200

    Delete app directory

--- app/supabase_client.py
+++
@@ -1,8 +0,0 @@
-from supabase import create_client, Client
-from app.config import Config
-
-
-def get_supabase() -> Client:
-    if not Config.SUPABASE_URL or not Config.SUPABASE_KEY:
-        raise ValueError("SUPABASE_URL e SUPABASE_SERVICE_KEY devono essere impostate")
-    return create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
